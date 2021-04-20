1e0db97c83a6642ad904f436a4d6f5b1f9c8998b
wira-tl <wira@tinkerlust.com>
2021-04-20 21:46:07 +0700

    add main file

---
+++ main.rb
@@ -0,0 +1,12 @@
+require_relative './poem.rb'
+
+## UNCOMMENT TO START TESTING
+
+poem = Poem.new
+# puts(poem.recite(1))
+# puts(poem.recite(12))
+# puts(poem.recite(-1))
+# puts(poem.recite(12))
+# puts(poem.recite_random)
+# puts(poem.recite_subject(12))
+# puts(poem.recite_random_subject)
